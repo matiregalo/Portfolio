7974d850a72a632c63275db002c079d51374fe97
matiregalo <matiasregalo64@gmail.com>
2026-01-03 07:22:38 -0300

    Finalización de front de contact

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "./index.css";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
